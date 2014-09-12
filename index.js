541ec6ac3e1311a707ceef2a8225f0d83ca02cb0
alee <alee@chou.im>
2014-09-13 01:27:26 +0800

    libqqbot

---
+++ index.js
@@ -0,0 +1,7 @@
+require('coffee-script/register')
+
+exports.auth      = require("./src/qqauth")
+exports.api       = require("./src/qqapi")
+exports.QQBot     = require("./src/qqbot")
+exports.defaults  = require('./src/defaults')
+// var config    = require('./config')
